34417d8582fc51885eaecfe2ae79c3628697aa30
Nicolas Rundstadler <n.rundstadler@laposte.net>
2024-11-19 10:17:46 +0100

    Initial commit with basic site design (excluding filter dropdowns and tags)

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html, js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ["Manrope", "sans-serif"],
+        display: ["Anton", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
