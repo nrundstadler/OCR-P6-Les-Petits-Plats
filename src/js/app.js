089518998d9a32499dc61592713e54e010589735
Nicolas Rundstadler <n.rundstadler@laposte.net>
2024-11-19 17:20:23 +0100

    Implement search with functional methods

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,13 @@ import { RecipesController } from "./controllers/RecipesController.js";
 function initApp() {
   const recipesController = new RecipesController(recipesData);
 
+  // Instant search (triggered as soon as the user types)
+  const searchInput = document.getElementById("recipes-search");
+  searchInput.addEventListener("input", (e) => {
+    recipesController.searchRecipes(e.target.value);
+  });
+
+  // Display all recipes by default
   recipesController.showRecipes();
 }
 
--- src/js/controllers/RecipesController.js
+++ src/js/controllers/RecipesController.js
@@ -4,10 +4,32 @@ import { renderRecipesCounter } from "../components/recipesCounter.js";
 export class RecipesController {
   constructor(recipes) {
     this.recipes = recipes;
+    this.filteredRecipes = recipes;
   }
 
   showRecipes() {
-    renderRecipesList(this.recipes);
-    renderRecipesCounter(this.recipes.length);
+    renderRecipesList(this.filteredRecipes);
+    renderRecipesCounter(this.filteredRecipes.length);
+  }
+
+  searchRecipes(query) {
+    // Remove spaces before/after and convert query to lowercase
+    query = query.trim().toLowerCase();
+
+    // If query is less than 3 characters, show all recipes
+    if (query.length < 3) {
+      this.filteredRecipes = this.recipes;
+    } else {
+      this.filteredRecipes = this.recipes.filter((recipe) => {
+        // Check if any of the criteria match, return true immediately when found
+        return (
+          recipe.name.toLowerCase().includes(query) ||
+          recipe.description.toLowerCase().includes(query) ||
+          recipe.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(query))
+        );
+      });
+    }
+
+    this.showRecipes(); // Show filtered recipes
   }
 }
