e3e363d064538d35b15ec851db26149a83e8a448
Nicolas Rundstadler <n.rundstadler@laposte.net>
2024-11-21 15:21:05 +0100

    refactor: improve search functionality by separating logic into helper methods (cleanQuery, isInName, isInDescription, isInIngredients) and adding XSS protection with query sanitization

--- src/js/controllers/RecipesController.js
+++ src/js/controllers/RecipesController.js
@@ -3,8 +3,8 @@ import { renderRecipesCounter } from "../components/recipesCounter.js";
 
 export class RecipesController {
   constructor(recipes) {
-    this.recipes = recipes;
-    this.filteredRecipes = recipes;
+    this.recipes = recipes; // Initial list of all recipes
+    this.filteredRecipes = recipes; // List of filtered recipes, initially equal to all recipes
   }
 
   showRecipes() {
@@ -13,8 +13,7 @@ export class RecipesController {
   }
 
   searchRecipes(query) {
-    // Remove spaces before/after and convert query to lowercase
-    query = query.trim().toLowerCase();
+    query = this.cleanQuery(query);
 
     // If query is less than 3 characters, show all recipes
     if (query.length < 3) {
@@ -23,13 +22,32 @@ export class RecipesController {
       this.filteredRecipes = this.recipes.filter((recipe) => {
         // Check if any of the criteria match, return true immediately when found
         return (
-          recipe.name.toLowerCase().includes(query) ||
-          recipe.description.toLowerCase().includes(query) ||
-          recipe.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(query))
+          this.isInName(recipe, query) || this.isInDescription(recipe, query) || this.isInIngredients(recipe, query)
         );
       });
     }
 
     this.showRecipes(); // Show filtered recipes
+  }
+
+  // Helper method to clean the query
+  // (remove extra spaces, convert to lowercase, and prevent XSS attacks)
+  cleanQuery(query) {
+    return query.trim().toLowerCase().replace(/[<>]/g, "");
+  }
+
+  // Helper method to check if the query matches the recipe name.
+  isInName(recipe, query) {
+    return recipe.name.toLowerCase().includes(query);
+  }
+
+  // Helper method to check if the query matches the recipe description.
+  isInDescription(recipe, query) {
+    return recipe.description.toLowerCase().includes(query);
+  }
+
+  // Helper method to check if the query matches any ingredient in the recipe.
+  isInIngredients(recipe, query) {
+    return recipe.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(query));
   }
 }
